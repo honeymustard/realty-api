569bc30fc4b73453c107bd3e50ba3e81f6e628e5
Adrian Solumsmo <adrian.solumsmo@gmail.com>
2018-09-09 21:01:03 +0200

    implements initial parsing routine

--- Controllers/TestController.cs
+++ Controllers/TestController.cs
@@ -1,12 +1,15 @@
 using System;
 using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Honeymustard
 {
     [Produces("application/json")]
     [Route("api/[controller]")]
-    public class TestController
+    public class TestController : Controller
     {
         protected IPathService Paths;
 
@@ -22,11 +25,28 @@ namespace Honeymustard
             return HTTP.Fetch(new Uri("https://www.nrk.no"));
         }
 
-        // GET api/test/parse
-        [HttpGet("parse")]
-        public string Parse()
+        // GET api/test/show
+        [HttpGet("show/{name}")]
+        public string Show(string name)
         {
-            return File.ReadAllText(Path.Combine(Paths.GetDataPath(), "published-today.html"));
+            return Utils.ReadFile(Paths.GetDataPath(), $"{name}.html");
+        }
+
+        // GET api/test/parse/today
+        [HttpGet("parse/today")]
+        public IActionResult Parse()
+        {
+            var file = Utils.ReadFile(Paths.GetDataPath(), "today.html");
+
+            var container = "<div class=\"unit flex align-items-stretch result-item\">";
+
+            var parser = new Parser(file);
+            var indices = parser.FindIndices(container);
+            var partitions = parser.Partition(indices);
+            var chunks = parser.Chunk(partitions);
+            var models = chunks.Select(e => new RealtyParser().Parse(e)).ToList();
+
+            return Json(models);
         }
     }
 }
