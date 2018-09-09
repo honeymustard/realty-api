90580cd95c7b36c9d4352cef7cb10ed86d26af79
Adrian Solumsmo <adrian.solumsmo@gmail.com>
2018-09-09 11:11:33 +0200

    adds model for realty item

---
+++ Models/RealtyModel.cs
@@ -0,0 +1,14 @@
+namespace Honeymustard
+{
+    public class RealtyModel
+    {
+        public string ID { get; set; }
+        public string ImageUri { get; set; }
+        public string Address { get; set; }
+        public string Description { get; set; }
+        public int SquareMeters { get; set; }
+        public int Price { get; set; }
+        public int SharedDept { get; set; }
+        public int SharedExpenses { get; set; }
+    }
+}
