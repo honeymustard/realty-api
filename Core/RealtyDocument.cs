480b5a5cde7dfd91ee35ea388f1d1db93089ec35
Adrian Solumsmo <adrian.solumsmo@gmail.com>
2018-09-14 20:03:35 +0200

    enables mogodb identity string in realty documents

--- Core/RealtyDocument.cs
+++ Core/RealtyDocument.cs
@@ -6,7 +6,8 @@ namespace Honeymustard
 {
     public class RealtyDocument
     {
-        public ObjectId Id { get; set; }
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
 
         [BsonElement]
         public string RealtyId { get; set; }
