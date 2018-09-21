30b20516ac91d4a9fada6c15ea0d1faf98c91397
Adrian Solumsmo <adrian.solumsmo@gmail.com>
2018-09-21 20:05:23 +0200

    adds new user model

---
+++ Models/UserModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Honeymustard
+{
+    public class UserModel
+    {
+        public string ID { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
