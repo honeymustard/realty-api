03d0baa46b81ca5d508854507c575c43119bf580
Adrian Solumsmo <adrian.solumsmo@gmail.com>
2018-09-21 23:00:47 +0200

    adds user repository

---
+++ Repositories/UserRepository.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Collections.Generic;
+using MongoDB.Driver;
+using MongoDB.Bson;
+
+namespace Honeymustard
+{
+    public class UserRepository : IRepository<UserDocument>
+    {
+        protected IMongoDatabase Db { get; set; }
+        protected IMongoCollection<UserDocument> Collection { get; set; }
+
+        public UserRepository(IDatabase database)
+        {
+            Db = database.GetDatabase();
+            Collection = Db.GetCollection<UserDocument>("users");
+        }
+
+        public static FilterDefinition<UserDocument> FilterMatch(UserDocument user)
+        {
+            var builder = Builders<UserDocument>.Filter;
+            return builder.And(
+                builder.Eq("username", user.Username),
+                builder.Eq("password", user.Password)
+            );
+        }
+
+        public IEnumerable<UserDocument> GetAll()
+        {
+            return Collection.Find(new BsonDocument()).ToEnumerable();
+        }
+
+        public void InsertOne(UserDocument document)
+        {
+            Collection.InsertOne(document);
+        }
+
+        public IEnumerable<UserDocument> InsertMany(IEnumerable<UserDocument> documents)
+        {
+            Collection.InsertMany(documents);
+            return documents;
+        }
+
+        public IEnumerable<UserDocument> FindAny(FilterDefinition<UserDocument> filter)
+        {
+            return Collection.Find(filter).ToEnumerable();
+        }
+    }
+}
