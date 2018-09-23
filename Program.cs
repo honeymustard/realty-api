9c5ca75a0e551cbc2da7cf507bafbc03b19ed99e
Adrian Solumsmo <adrian.solumsmo@gmail.com>
2018-09-23 18:36:40 +0200

    simplifies program class

--- Program.cs
+++ Program.cs
@@ -14,13 +14,11 @@ namespace Honeymustard
     {
         public static void Main(string[] args)
         {
-            BuildWebHost(args).Run();
-        }
-
-        public static IWebHost BuildWebHost(string[] args) =>
             WebHost
                 .CreateDefaultBuilder(args)
                 .UseStartup<Startup>()
-                .Build();
+                .Build()
+                .Run();
+        }
     }
 }
